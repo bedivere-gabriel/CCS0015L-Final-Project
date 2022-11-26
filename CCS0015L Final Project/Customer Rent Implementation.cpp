3b8edff08611f80e01d2bde632ccea2ad71b29a0
hanxshi <jiyuni0011@gmail.com>
2022-11-26 17:28:38 +0800

    added empty functions //no code yet

---
+++ CCS0015L Final Project/Customer Rent Implementation.cpp
@@ -0,0 +1,20 @@
+#include "Customer Rent.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void CustomerRentDataStructure::rentVideo(int customer_id, int video_id) {
+
+}
+
+
+void CustomerRentDataStructure::returnVideo(int customer_id, int video_id) {
+
+}
+
+
+
+void CustomerRentDataStructure::showRentedVideoList(int customer_id) {
+	
+}
