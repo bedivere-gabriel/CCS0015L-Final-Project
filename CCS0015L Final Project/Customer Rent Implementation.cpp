6ac7238a2a9007029d5c4da10b10318404828744
hanxshi <jiyuni0011@gmail.com>
2022-11-26 20:56:30 +0800

    restarting functions lmao

--- CCS0015L Final Project/Customer Rent Implementation.cpp
+++ CCS0015L Final Project/Customer Rent Implementation.cpp
@@ -5,38 +5,20 @@
 using namespace std;
 
 void CustomerRentDataStructure::rentVideo(int customer_id, int video_id) {
-	if (!customer_id) {
-
-	}
-	else {
-		
-	}
+	Node* nodePtr;
+	
 }
 
 
 void CustomerRentDataStructure::returnVideo(int customer_id, int video_id) {
-	int x;
-	for (x = 0; x < 10; x++) {
-		if (customer_id == x) {
-			
-		}
-	}
+	Node* nodePtr;
+	
+
 }
 
 
 
 void CustomerRentDataStructure::showRentedVideoList(int customer_id) {
 	Node* nodePtr;
-	if (head == NULL) {
-		cout << "ID doesn't exist";
-	}
-	else {
-		nodePtr = head;
-		while (nodePtr) {
-			if (nodePtr->_id == customer_id) {
-				//display info
-			}
-			nodePtr = nodePtr->next;
-		}
-	}
+
 }
