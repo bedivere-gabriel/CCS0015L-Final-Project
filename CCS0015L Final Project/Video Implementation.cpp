ccc17f2bac7d2c2451d849ed55a28b547cc82b70
hanxshi <jiyuni0011@gmail.com>
2022-11-27 17:02:42 +0800

    Revert "attempted return/rent functions lol"
    This reverts commit b9918420d403e4e368b86124c4968e72b8e92099.

--- CCS0015L Final Project/Video Implementation.cpp
+++ CCS0015L Final Project/Video Implementation.cpp
@@ -51,39 +51,9 @@ void VideoDataStructure::insertVideo(int id, string title, string genre, string 
 }
 
 void VideoDataStructure::rentVideo(int id, int copies){
-	node* nodePtr;
-	nodePtr = head;
 
-	if (!head) {
-		cout << "Empty list" << endl;
-	}
-
-	else {
-		while (nodePtr->next) {
-			if (nodePtr->_id == id) {
-				nodePtr->_copies--;
-				copies--;
-			}
-			nodePtr->next;
-		}
-	}
 }
 
-void VideoDataStructure::returnVideo(int id, int copies){
-	node* nodePtr;
-	nodePtr = head;
+void VideoDataStructure::returnVideo(int id){
 
-	if (!head) {
-		cout << "Empty list" << endl;
-	}
-
-	else {
-		while (nodePtr->next) {
-			if (nodePtr->_id == id) {
-				nodePtr->_copies++;
-				copies++;
-			}
-			nodePtr->next;
-		}
-	}
 }
